c8525f3edcd5012f1fa6246cecd5ce0156697aab
SiveyDev <sivey@protonmail.com>
2021-10-22 19:08:47 -0400

    Add and test multiply() function

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 console.log("My favorite pie is 3.14");
 
-add(7,60);
+add(7.1,60);
 subtract(10,3);
 subtract(-3,10);
+multiply(-3,-4);
 
 function add(x, y) {
     console.log(x + y);
@@ -10,4 +11,8 @@ function add(x, y) {
 
 function subtract(x, y) {
     console.log(x - y);
+}
+
+function multiply(x, y) {
+    console.log(x * y);
 }
