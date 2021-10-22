57e7f16be40400f7caf0f0f89086d3220250b3ce
SiveyDev <sivey@protonmail.com>
2021-10-22 19:04:18 -0400

    Add and test subtract() function

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
 console.log("My favorite pie is 3.14");
 
 add(7,60);
+subtract(10,3);
+subtract(-3,10);
 
 function add(x, y) {
     console.log(x + y);
+}
+
+function subtract(x, y) {
+    console.log(x - y);
 }
