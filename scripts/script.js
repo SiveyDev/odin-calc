fb77544219f68ae146b77a6a4ac7dee1e36eb5c3
SiveyDev <sivey@protonmail.com>
2021-10-22 19:12:11 -0400

    Add and test divide() function

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ add(7.1,60);
 subtract(10,3);
 subtract(-3,10);
 multiply(-3,-4);
+divide(-20,-5);
 
 function add(x, y) {
     console.log(x + y);
@@ -15,4 +16,8 @@ function subtract(x, y) {
 
 function multiply(x, y) {
     console.log(x * y);
+}
+
+function divide(x, y) {
+    console.log(x / y);
 }
