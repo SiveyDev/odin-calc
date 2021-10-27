54752e3ebf463b3818efd82946227e199563e103
SiveyDev <sivey@protonmail.com>
2021-10-26 20:54:47 -0400

    Add event listener to buttons and create a function to show the clicked numbers on the display

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
-console.log("My favorite pie is 3.14");
-
-operate(divide, 10, 5);
+const display = document.querySelector('.calc-display');
+const numbtns = document.querySelectorAll('.num');
+let displayValue = [];
+numbtns.forEach(numbtn => addEventListener('click', getNum));
 
 function add(num1, num2) {
     console.log(num1 + num2);
@@ -20,4 +21,9 @@ function divide(num1, num2) {
 
 function operate(funcName, num1, num2) {
     funcName(num1, num2);
+}
+
+function getNum(e) {
+    displayValue.push(e.target.textContent);
+    display.textContent += e.target.textContent;
 }
